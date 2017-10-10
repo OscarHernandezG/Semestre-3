27171b8b7060e19c40a2ed40be7b996f99e181c3
Óscar Hernández <oscarhernandezgomez@gmail.com>
2017-10-10 19:59:28 +0200

    map

---
+++ Desarrollo/Dev_class5_handout/Game/maps/sin_nombre.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="MapaTileset" tilewidth="256" tileheight="256" spacing="1" margin="1" tilecount="28" columns="7">
+ <image source="MapaTileset.png" width="1800" height="1029"/>
+</tileset>
